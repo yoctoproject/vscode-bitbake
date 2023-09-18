e1d9088d6f7cabb75f3efa958b6d5b8a9be24311
idillon <io.daza-dillon@savoirfairelinux.com>
2023-09-18 13:26:55 -0400

    Refactor: Add eslint to client

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -2,56 +2,57 @@
  * Copyright (c) Eugen Wiens. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
-'use strict';
 
-import * as path from 'path';
+import * as path from 'path'
 
-import { workspace, ExtensionContext } from 'vscode';
-import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient/node';
+import { workspace } from 'vscode'
+import { LanguageClient, TransportKind } from 'vscode-languageclient/node'
 
-let client: LanguageClient;
-export function activate(context: ExtensionContext) {
-	console.log('Congratulations, your extension "BitBake" is now active!');
-	let serverModule = context.asAbsolutePath(path.join('../server', 'out','server.js'));
-	// The debug options for the server
-	let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+import type { ExtensionContext } from 'vscode'
+import type { LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node'
 
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
-	let serverOptions: ServerOptions = {
-		run : { module: serverModule, transport: TransportKind.ipc },
-		debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
-	}
-	
-	// Options to control the language client
-	let clientOptions: LanguageClientOptions = {
-		// Register the server for bitbake documents
-        // TODO: check new documentSelector
-		documentSelector: [{scheme: 'file', language: 'bitbake'}],
-		synchronize: {
-			configurationSection: 'bitbake',
+let client: LanguageClient
+export async function activate (context: ExtensionContext): Promise<void> {
+  console.log('Congratulations, your extension "BitBake" is now active!')
+  const serverModule = context.asAbsolutePath(path.join('../server', 'out', 'server.js'))
+  // The debug options for the server
+  const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] }
 
-			// Notify the server about file changes to '.clientrc files contain in the workspace
-			fileEvents: [
-				workspace.createFileSystemWatcher('**/*.bbclass', false, true, false),
-				workspace.createFileSystemWatcher('**/*.inc', false, true, false),
-				workspace.createFileSystemWatcher('**/*.bb', false, true, false),
-				workspace.createFileSystemWatcher('**/*.conf', false, true, false)
-			]
-		}
-	}
-	
-	// Create the language client and start the client.
-	client = new LanguageClient('bitbake', 'Language Server Bitbake', serverOptions, clientOptions)
-	
-	// Start the client and launch the server
-	client.start();
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  const serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
+  }
+
+  // Options to control the language client
+  const clientOptions: LanguageClientOptions = {
+    // Register the server for bitbake documents
+    // TODO: check new documentSelector
+    documentSelector: [{ scheme: 'file', language: 'bitbake' }],
+    synchronize: {
+      configurationSection: 'bitbake',
+
+      // Notify the server about file changes to '.clientrc files contain in the workspace
+      fileEvents: [
+        workspace.createFileSystemWatcher('**/*.bbclass', false, true, false),
+        workspace.createFileSystemWatcher('**/*.inc', false, true, false),
+        workspace.createFileSystemWatcher('**/*.bb', false, true, false),
+        workspace.createFileSystemWatcher('**/*.conf', false, true, false)
+      ]
+    }
+  }
+
+  // Create the language client and start the client.
+  client = new LanguageClient('bitbake', 'Language Server Bitbake', serverOptions, clientOptions)
+
+  // Start the client and launch the server
+  await client.start()
 }
 
-export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-	  return undefined;
-	}
-	return client.stop();
+export function deactivate (): Thenable<void> | undefined {
+  if (client === undefined) {
+    return undefined
+  }
+  return client.stop()
 }
-  
