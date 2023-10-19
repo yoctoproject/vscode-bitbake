0882ec0844f03256fad4bd5e8da8dff3195258f8
Enguerrand de Ribaucourt <enguerrand.de-ribaucourt@savoirfairelinux.com>
2023-10-19 12:08:33 +0200

    Test: check JS copyright header
    The eslint plugin is also added to package.json. package-lock.json is
    updated with npm 20.8.0.

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,9 +21,17 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  rules: {},
+  plugins: ['header'],
+  rules: {
+    "header/header": [2, "block", [
+      " --------------------------------------------------------------------------------------------",
+      {"pattern": " \\* Copyright \\(c\\) .*\\. All rights reserved\\.", "template": " * Copyright (c) 2023 Savoir-faire Linux. All rights reserved."},
+      " * Licensed under the MIT License. See License.txt in the project root for license information.",
+      " * ------------------------------------------------------------------------------------------ ",
+    ], 2]
+  },
   ignorePatterns: [
     'out',
     '__mocks__/vscode.ts'
-  ]
+  ],
 }
