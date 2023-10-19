a396aafea60cd60f13dbe356034b4205bec62892
Ziwei Wang <ziweiwzw217@gmail.com>
2023-10-19 12:49:01 -0400

    Chore: Add tests for suggestions in string quotations

--- server/src/__tests__/completions.test.ts
+++ server/src/__tests__/completions.test.ts
@@ -1,9 +1,30 @@
 import { onCompletionHandler } from '../connectionHandlers/onCompletion'
+import { analyzer } from '../tree-sitter/analyzer'
+import { FIXTURE_DOCUMENT } from './fixtures/fixtures'
+import { generateParser } from '../tree-sitter/parser'
 
 const DUMMY_URI = 'dummy_uri'
 
+/**
+ * The onCompletion handler doesn't allow other parameters, so we can't pass the analyzer and therefore the same
+ * instance used in the handler is used here. Documents are reset before each test for a clean state.
+ * A possible alternative is making the entire server a class and the analyzer a member
+ */
 describe('On Completion', () => {
+  beforeAll(async () => {
+    if (!analyzer.hasParser()) {
+      const parser = await generateParser()
+      analyzer.initialize(parser)
+    }
+    analyzer.resetAnalyzedDocuments()
+  })
+
+  beforeEach(() => {
+    analyzer.resetAnalyzedDocuments()
+  })
+
   it('expects reserved variables, keywords and snippets in completion item lists', async () => {
+    // nothing is analyzed yet, only the static completion items are provided
     const result = onCompletionHandler({
       textDocument: {
         uri: DUMMY_URI
@@ -53,5 +74,43 @@ describe('On Completion', () => {
         }
       ])
     )
+  })
+
+  it("doesn't provide suggestions when it is pure string content", async () => {
+    await analyzer.analyze({
+      uri: DUMMY_URI,
+      document: FIXTURE_DOCUMENT.COMPLETION
+    })
+
+    const result = onCompletionHandler({
+      textDocument: {
+        uri: DUMMY_URI
+      },
+      position: {
+        line: 1,
+        character: 10
+      }
+    })
+
+    expect(result).toEqual([])
+  })
+
+  it('provides suggestions when it is in variable expansion', async () => {
+    await analyzer.analyze({
+      uri: DUMMY_URI,
+      document: FIXTURE_DOCUMENT.COMPLETION
+    })
+
+    const result = onCompletionHandler({
+      textDocument: {
+        uri: DUMMY_URI
+      },
+      position: {
+        line: 1,
+        character: 13
+      }
+    })
+
+    expect(result).not.toEqual([])
   })
 })
