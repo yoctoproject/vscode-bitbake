6d987433f71a4df59a51b0752550044b82567be0
Enguerrand de Ribaucourt <enguerrand.de-ribaucourt@savoirfairelinux.com>
2023-10-25 16:18:32 +0200

    Test: Add unit test for BitbakeWorkspace

---
+++ client/src/__tests__/unit-tests/bitbake-workspace.test.ts
@@ -0,0 +1,19 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) 2023 Savoir-faire Linux. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import { BitbakeWorkspace } from '../../ui/BitbakeWorkspace'
+
+describe('Bitbake Workspace Test Suite', () => {
+  const bitbakeWorkspace = new BitbakeWorkspace()
+
+  it('should manage a list of active recipes', async () => {
+    expect(bitbakeWorkspace.activeRecipes).toEqual([])
+    bitbakeWorkspace.addActiveRecipe('foo')
+    expect(bitbakeWorkspace.activeRecipes).toEqual(['foo'])
+
+    bitbakeWorkspace.dropActiveRecipe('foo')
+    expect(bitbakeWorkspace.activeRecipes).toEqual([])
+  })
+})
