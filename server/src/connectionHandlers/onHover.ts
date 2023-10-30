3edf1b5cba3732d71c5c8cbc93ccb5154b92f081
Ziwei Wang <ziweiwzw217@gmail.com>
2023-10-30 12:07:55 -0400

    Fix: Bug-10182 definition show on presence at all times even word is a part of another word
    Now it only shows bitbake variable definition on global declaration syntax
    e.g. VAR in VAR = '123' but not in python VAR(){} or FOO = '${VAR}'

--- server/src/connectionHandlers/onHover.ts
+++ server/src/connectionHandlers/onHover.ts
@@ -6,40 +6,47 @@
 import { type HoverParams, type Hover } from 'vscode-languageserver'
 import { analyzer } from '../tree-sitter/analyzer'
 import { bitBakeDocScanner } from '../BitBakeDocScanner'
+import logger from 'winston'
 
-export async function onHoverHandler (params: HoverParams): Promise<Hover | undefined> {
+export async function onHoverHandler (params: HoverParams): Promise<Hover | null> {
   const { position, textDocument } = params
-  const documentAsText = analyzer.getDocumentTexts(textDocument.uri)
-  const textLine = documentAsText?.[position.line]
-  if (textLine === undefined) {
-    return undefined
+  logger.debug(`[onHover] document uri: ${textDocument.uri} position: Line ${position.line} Column ${position.character}`)
+  const word = analyzer.wordAtPoint(textDocument.uri, position.line, position.character)
+  if (word === null) {
+    return null
   }
-  const matches = textLine.matchAll(bitBakeDocScanner.variablesRegex)
-  for (const match of matches) {
-    const name = match[1].toUpperCase()
-    if (name === undefined || match.index === undefined) {
-      continue
+  // Show documentation of a bitbake variable
+  // Triggers only on global declaration expressions like "VAR = 'foo'" but skip the ones like "python VAR(){}"
+  const wordIsGlobalDeclarationAndVariableName: boolean = analyzer.getGlobalDeclarationSymbols(textDocument.uri).some((symbol) => symbol.name === word) && analyzer.isIdentifierOfVariableAssignment(params)
+  if (wordIsGlobalDeclarationAndVariableName) {
+    const found = Object.keys(bitBakeDocScanner.variablesInfos).includes(word)
+    if (!found) {
+      logger.debug(`[onHover] Not a bitbake variable: ${word}`)
+      return null
     }
-    const start = match.index
-    const end = start + name.length
+    logger.debug(`[onHover] Found bitbake variable: ${word}`)
+    const range = analyzer.rangeForWordAtPoint(params)
+    if (range === undefined) {
+      logger.debug(`[onHover] Can't find the range for word: ${word}`)
+      return null
+    }
+    const start = range.start.character
+    const end = range.end.character
     if ((start > position.character) || (end <= position.character)) {
-      continue
+      logger.debug(`[onHover] Invalid position: Line: ${position.line} Character: ${position.character}`)
+      return null
     }
 
-    const definition = bitBakeDocScanner.variablesInfos[name]?.definition
+    const definition = bitBakeDocScanner.variablesInfos[word]?.definition
     const hover: Hover = {
       contents: {
         kind: 'markdown',
-        value: `**${name}**\n___\n${definition}`
-      },
-      range: {
-        start: position,
-        end: {
-          ...position,
-          character: end
-        }
+        value: `**${word}**\n___\n${definition}`
       }
     }
+    logger.debug(`[onHover] Hover item: ${JSON.stringify(hover)}`)
     return hover
   }
+
+  return null
 }
