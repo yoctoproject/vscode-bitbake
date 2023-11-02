29ad03aa7e8fa8b06ff9ac08b64c43193b84042c
Enguerrand de Ribaucourt <enguerrand.de-ribaucourt@savoirfairelinux.com>
2023-11-02 13:50:37 +0100

    Test: test BitbBakeProjectScanner
    Add an integration test that runs bitbake and ensures we properly
    parse the result to collect statistics about the project.

---
+++ server/src/__tests__/scanner.test.ts
@@ -0,0 +1,84 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) 2023 Savoir-faire Linux. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import bitBakeProjectScanner from '../BitBakeProjectScanner'
+import path from 'path'
+
+const pathToBitbakeFolder = path.join(__dirname, '../../../integration-tests/project-folder/sources/poky/bitbake')
+const pathToBuildFolder = path.join(__dirname, '../../../integration-tests/project-folder/build')
+const pathToEnvScript = path.join(__dirname, '../../../integration-tests/project-folder/sources/poky/oe-init-build-env')
+const workspaceFolder = path.join(__dirname, '../../../integration-tests/project-folder')
+
+describe('BitBakeProjectScanner', () => {
+  beforeAll(() => {
+    bitBakeProjectScanner.loadSettings(
+      {
+        pathToBitbakeFolder,
+        pathToBuildFolder,
+        pathToEnvScript
+      },
+      workspaceFolder
+    )
+    bitBakeProjectScanner.rescanProject()
+  })
+
+  it('can run a bitbake parse', async () => {
+    const parseSuccesful = bitBakeProjectScanner.parseAllRecipes()
+    expect(parseSuccesful).toBe(true)
+  })
+
+  it('can get a list of layers', async () => {
+    const layers = bitBakeProjectScanner.layers
+    // poky provides the "core", "yocto" and "yoctobsp" layers
+    expect(layers.length).toBeGreaterThan(2)
+    expect(layers).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining(
+          {
+            name: 'core'
+          }
+        )
+      ])
+    )
+  })
+
+  it('can get a list of recipes', async () => {
+    const recipes = bitBakeProjectScanner.recipes
+    expect(recipes.length).toBeGreaterThan(100)
+    expect(recipes).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining(
+          {
+            name: 'core-image-minimal'
+          }
+        )
+      ])
+    )
+  })
+
+  it('can get a list of classes', async () => {
+    const classes = bitBakeProjectScanner.classes
+    expect(classes.length).toBeGreaterThan(50)
+    expect(classes).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining(
+          {
+            name: 'image'
+          }
+        )
+      ])
+    )
+  })
+
+  it('can get a list of overrides', async () => {
+    const overrides = bitBakeProjectScanner.overrides
+    expect(overrides.length).toBeGreaterThan(5)
+    expect(overrides).toEqual(
+      expect.arrayContaining([
+        'class-target'
+      ])
+    )
+  })
+})
