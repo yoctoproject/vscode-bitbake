35f86e4202d56dbd331322212a06825bba8f83b1
Ziwei Wang <ziweiwzw217@gmail.com>
2023-11-07 10:34:58 -0500

    Chore: Specify a commit for the docs

--- scripts/fetch-docs.sh
+++ scripts/fetch-docs.sh
@@ -6,6 +6,8 @@ mkdir -p resources/docs
 git clone --depth 1 --filter=blob:none --sparse https://github.com/openembedded/bitbake.git 
 cd bitbake 
 git sparse-checkout set doc/bitbake-user-manual/ 
+git fetch origin
+git checkout 595176d6be95a9c4718d3a40499d1eb576b535f5
 cd doc/bitbake-user-manual/ 
 mv bitbake-user-manual-metadata.rst bitbake-user-manual-ref-variables.rst  ../../../resources/docs 
 cd ../../../ 
@@ -15,6 +17,8 @@ rm -rf bitbake
 git clone --depth 1 --filter=blob:none --sparse https://git.yoctoproject.org/yocto-docs 
 cd yocto-docs 
 git sparse-checkout set documentation/ref-manual 
+git fetch origin
+git checkout 897d5017eae6b3af2d5d489fc4e0915d9ce21458
 cd documentation/ref-manual 
 mv tasks.rst ../../../resources/docs 
 cd ../../../ 
