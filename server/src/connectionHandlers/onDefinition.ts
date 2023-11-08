cbea0cbbf88aee3e559085cbfd39340d738e1f26
Ziwei Wang <ziweiwzw217@gmail.com>
2023-11-08 15:14:49 -0500

    Chore: Add debug message when document text is undefined

--- server/src/connectionHandlers/onDefinition.ts
+++ server/src/connectionHandlers/onDefinition.ts
@@ -14,6 +14,7 @@ export function onDefinitionHandler (textDocumentPositionParams: TextDocumentPos
 
   const documentAsText = analyzer.getDocumentTexts(textDocument.uri)
   if (documentAsText === undefined) {
+    logger.debug(`[onDefinition] Document not found for ${textDocument.uri}`)
     return []
   }
 
