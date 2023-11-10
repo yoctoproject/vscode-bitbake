a9123b16df0efece5d266f9f57a79d5b6d5a14d1
Ziwei Wang <ziweiwzw217@gmail.com>
2023-11-10 13:49:59 -0500

    Chore: Remove dummy test

--- server/src/__tests__/test.test.ts
+++
@@ -1,8 +0,0 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) 2023 Savoir-faire Linux. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
-
-test('two plus two is four', () => {
-  expect(2 + 2).toBe(4)
-})
