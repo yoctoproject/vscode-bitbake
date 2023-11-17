2e65564f4e07b287d3230880886ea9f5727929aa
Ziwei Wang <ziweiwzw217@gmail.com>
2023-11-17 16:40:34 -0500

    Chore: Move functions from contextHandler to here

--- server/src/connectionHandlers/onDefinition.ts
+++ server/src/connectionHandlers/onDefinition.ts
@@ -6,7 +6,8 @@
 import { logger } from '../lib/src/utils/OutputLogger'
 import { type TextDocumentPositionParams, type Definition } from 'vscode-languageserver/node'
 import { analyzer } from '../tree-sitter/analyzer'
-import contextHandler from '../ContextHandler'
+import { type DirectiveStatementKeyword } from '../lib/src/types/directiveKeywords'
+import { definitionProvider } from '../DefinitionProvider'
 
 export function onDefinitionHandler (textDocumentPositionParams: TextDocumentPositionParams): Definition | null {
   const { textDocument, position } = textDocumentPositionParams
@@ -22,10 +23,81 @@ export function onDefinitionHandler (textDocumentPositionParams: TextDocumentPos
   const directiveStatementKeyword = analyzer.getDirectiveStatementKeywordByNodeType(textDocumentPositionParams)
   if (directiveStatementKeyword !== undefined) {
     logger.debug(`[onDefinition] Found directive: ${directiveStatementKeyword}`)
-    const definition = contextHandler.getDefinitionForDirectives(directiveStatementKeyword, textDocumentPositionParams, documentAsText)
+    const definition = getDefinitionForDirectives(directiveStatementKeyword, textDocumentPositionParams, documentAsText)
     logger.debug(`[onDefinition] definition item: ${JSON.stringify(definition)}`)
     return definition
   }
 
-  return contextHandler.getDefinition(textDocumentPositionParams, documentAsText)
+  return getDefinition(textDocumentPositionParams, documentAsText)
+}
+
+function getDefinitionForDirectives (directiveStatementKeyword: DirectiveStatementKeyword, textDocumentPositionParams: TextDocumentPositionParams, documentAsText: string[]): Definition {
+  let definition: Definition = []
+  const currentLine: string = documentAsText[textDocumentPositionParams.position.line]
+  const symbol: string = extractSymbolFromLine(textDocumentPositionParams, currentLine)
+
+  const words: string[] = currentLine.split(' ')
+
+  if (words.length >= 2) {
+    if (words[0] === directiveStatementKeyword) {
+      logger.debug(`getDefinitionForKeyWord: ${JSON.stringify(words)}`)
+      if (words.length === 2) {
+        definition = definitionProvider.createDefinitionForKeyword(directiveStatementKeyword, words[1])
+      } else {
+        definition = definitionProvider.createDefinitionForKeyword(directiveStatementKeyword, words[1], symbol)
+      }
+    }
+  }
+  return definition
+}
+
+function getDefinition (textDocumentPositionParams: TextDocumentPositionParams, documentAsText: string[]): Definition {
+  let definition: Definition = []
+
+  const currentLine = documentAsText[textDocumentPositionParams.position.line]
+  const symbol = extractSymbolFromLine(textDocumentPositionParams, currentLine)
+
+  definition = definitionProvider.createDefinitionForSymbol(symbol)
+  return definition
+}
+
+function extractSymbolFromLine (textDocumentPositionParams: TextDocumentPositionParams, currentLine: string): string {
+  logger.debug(`getDefinitionForSymbol ${currentLine}`)
+  const linePosition: number = textDocumentPositionParams.position.character
+  let symbolEndPosition: number = currentLine.length
+  let symbolStartPosition: number = 0
+  const rightBorderCharacter: string[] = [' ', '=', '/', '$', '+', '}', '\'', '\'', ']', '[']
+  const leftBorderCharacter: string[] = [' ', '=', '/', '+', '{', '\'', '\'', '[', ']']
+
+  for (const character of rightBorderCharacter) {
+    let temp: number = currentLine.indexOf(character, linePosition)
+    if (temp === -1) {
+      temp = currentLine.length
+    }
+    symbolEndPosition = Math.min(symbolEndPosition, temp)
+  }
+
+  const symbolRightTrimed = currentLine.substring(0, symbolEndPosition)
+  logger.debug(`symbolRightTrimed ${symbolRightTrimed}`)
+
+  for (const character of leftBorderCharacter) {
+    let temp: number = symbolRightTrimed.lastIndexOf(character, linePosition)
+    if (temp === -1) {
+      temp = 0
+    }
+    symbolStartPosition = Math.max(symbolStartPosition, temp)
+  }
+
+  let symbol: string = symbolRightTrimed.substring(symbolStartPosition)
+
+  for (const character of leftBorderCharacter.concat('-')) {
+    if (symbol.startsWith(character)) {
+      symbol = symbol.substring(1)
+      break
+    }
+  }
+
+  logger.debug(`symbol ${symbol}`)
+
+  return symbol
 }
