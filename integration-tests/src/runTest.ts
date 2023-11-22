1794a130dd1e0f8d0a5dc0d475efba4a5df04340
idillon <io.daza-dillon@savoirfairelinux.com>
2023-11-22 14:18:17 -0500

    Chore: Add mads-hartmann.bash-ide-vscode and ms-python.python as dependencies

--- integration-tests/src/runTest.ts
+++ integration-tests/src/runTest.ts
@@ -3,12 +3,31 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
+import * as cp from 'child_process'
 import * as path from 'path'
-
-import { runTests } from '@vscode/test-electron'
+import {
+  downloadAndUnzipVSCode,
+  resolveCliArgsFromVSCodeExecutablePath,
+  runTests
+} from '@vscode/test-electron'
 
 async function main (): Promise<void> {
   try {
+    const vscodeExecutablePath = await downloadAndUnzipVSCode('1.84.2')
+    const [cliPath, ...args] = resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath)
+
+    cp.spawnSync(
+      cliPath,
+      [
+        ...args,
+        '--install-extension', 'mads-hartmann.bash-ide-vscode@1.39.0',
+        '--install-extension', 'ms-python.python@2023.20.0'
+      ],
+      {
+        encoding: 'utf-8',
+        stdio: 'inherit'
+      }
+    )
     // The folder containing the Extension Manifest package.json
     // Passed to `--extensionDevelopmentPath`
     const extensionDevelopmentPath = path.resolve(__dirname, '../../client/')
@@ -19,11 +38,17 @@ async function main (): Promise<void> {
 
     const testWorkspace = path.resolve(__dirname, '../../integration-tests/project-folder')
 
-    const launchArgs = ['--disable-extensions', '--disable-workspace-trust', testWorkspace]
+    const launchArgs = ['--disable-workspace-trust', testWorkspace]
     const extensionTestsEnv = {}
 
     // Download VS Code, unzip it and run the integration test
-    await runTests({ launchArgs, extensionDevelopmentPath, extensionTestsPath, extensionTestsEnv })
+    await runTests({
+      launchArgs,
+      vscodeExecutablePath,
+      extensionDevelopmentPath,
+      extensionTestsPath,
+      extensionTestsEnv
+    })
   } catch (err) {
     console.error(err)
     console.error('Failed to run tests')
