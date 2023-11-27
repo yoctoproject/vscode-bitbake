640033b11e97ad47865d6c7f41bba86d372dfed8
idillon <io.daza-dillon@savoirfairelinux.com>
2023-11-27 17:52:26 -0500

    Chore: EmbeddedLanguageDocsManager uses vscode api to manage files
    This gives better responsiveness when editing files

--- client/src/language/middlewareCompletion.ts
+++ client/src/language/middlewareCompletion.ts
@@ -3,12 +3,11 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-import { type CompletionList, Uri, commands, Range } from 'vscode'
+import { type CompletionList, commands, Range, workspace } from 'vscode'
 import { type CompletionMiddleware } from 'vscode-languageclient/node'
 
 import { requestsManager } from './RequestManager'
 import { getEmbeddedLanguageDocPosition, getOriginalDocRange } from './utils'
-import { getFileContent } from '../lib/src/utils/files'
 import { embeddedLanguageDocsManager } from './EmbeddedLanguageDocsManager'
 
 export const middlewareProvideCompletion: CompletionMiddleware['provideCompletionItem'] = async (document, position, context, token, next) => {
@@ -20,17 +19,14 @@ export const middlewareProvideCompletion: CompletionMiddleware['provideCompletio
   if (embeddedLanguageDocInfos === undefined || embeddedLanguageDocInfos === null) {
     return
   }
-  const embeddedLanguageDocContent = await getFileContent(Uri.parse(embeddedLanguageDocInfos.uri).fsPath)
-  if (embeddedLanguageDocContent === undefined) {
-    return
-  }
+  const embeddedLanguageTextDocument = await workspace.openTextDocument(embeddedLanguageDocInfos.uri)
   const adjustedPosition = getEmbeddedLanguageDocPosition(
     document,
-    embeddedLanguageDocContent,
+    embeddedLanguageTextDocument,
     embeddedLanguageDocInfos.characterIndexes,
     position
   )
-  const vdocUri = Uri.parse(embeddedLanguageDocInfos.uri)
+  const vdocUri = embeddedLanguageTextDocument.uri
   const result = await commands.executeCommand<CompletionList>(
     'vscode.executeCompletionItemProvider',
     vdocUri,
@@ -41,10 +37,10 @@ export const middlewareProvideCompletion: CompletionMiddleware['provideCompletio
     if (item.range === undefined) {
       // pass
     } else if (item.range instanceof Range) {
-      item.range = getOriginalDocRange(document, embeddedLanguageDocContent, embeddedLanguageDocInfos.characterIndexes, item.range)
+      item.range = getOriginalDocRange(document, embeddedLanguageTextDocument, embeddedLanguageDocInfos.characterIndexes, item.range)
     } else {
-      const inserting = getOriginalDocRange(document, embeddedLanguageDocContent, embeddedLanguageDocInfos.characterIndexes, item.range.inserting)
-      const replacing = getOriginalDocRange(document, embeddedLanguageDocContent, embeddedLanguageDocInfos.characterIndexes, item.range.replacing)
+      const inserting = getOriginalDocRange(document, embeddedLanguageTextDocument, embeddedLanguageDocInfos.characterIndexes, item.range.inserting)
+      const replacing = getOriginalDocRange(document, embeddedLanguageTextDocument, embeddedLanguageDocInfos.characterIndexes, item.range.replacing)
       if (inserting === undefined || replacing === undefined) {
         return
       }
--- client/src/lib/src/utils/files.ts
+++ client/src/lib/src/utils/files.ts
@@ -3,22 +3,7 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-import fs from 'fs'
 import path from 'path'
-
-import { logger } from './OutputLogger'
-
-export const getFileContent = async (path: string): Promise<string | undefined> => {
-  const fileContent = await new Promise<string>((resolve, reject) => {
-    fs.readFile(path, { encoding: 'utf-8' },
-      (error, data) => { error !== null ? reject(error) : resolve(data) }
-    )
-  }).catch(err => {
-    logger.error(`Could not open file: ${err}`)
-    return undefined
-  })
-  return fileContent
-}
 
 export function extractRecipeName (filename: string | undefined): string | undefined {
   if (filename === undefined) { return undefined }
