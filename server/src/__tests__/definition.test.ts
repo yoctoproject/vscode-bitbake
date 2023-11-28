22c7a8aff71cc704a7ef31d3c852958796bb7a22
Ziwei Wang <ziweiwzw217@gmail.com>
2023-11-28 10:47:29 -0500

    Chore: Add tests for go to definition for variables

--- server/src/__tests__/definition.test.ts
+++ server/src/__tests__/definition.test.ts
@@ -6,9 +6,11 @@
 import { analyzer } from '../tree-sitter/analyzer'
 import { generateParser } from '../tree-sitter/parser'
 import { onDefinitionHandler } from '../connectionHandlers/onDefinition'
-import { FIXTURE_DOCUMENT, DUMMY_URI } from './fixtures/fixtures'
+import { FIXTURE_DOCUMENT, DUMMY_URI, FIXTURE_URI } from './fixtures/fixtures'
 import { type Location } from 'vscode-languageserver'
 import { definitionProvider } from '../DefinitionProvider'
+import path from 'path'
+import { bitBakeProjectScannerClient } from '../BitbakeProjectScannerClient'
 // TODO: Current implementation of the definitionProvider needs to be improved, this test suite should be modified accordingly after
 const mockDefinition = (path: string | undefined): void => {
   if (path !== undefined) {
@@ -82,5 +84,74 @@ describe('on definition', () => {
     })
 
     expect(definition2).toEqual([])
+  })
+
+  it('provides go to definition for variables if the included files also contain the variable', async () => {
+    const parsedBazPath = path.parse(FIXTURE_DOCUMENT.BAZ_BBCLASS.uri.replace('file://', ''))
+    const parsedFooPath = path.parse(FIXTURE_DOCUMENT.FOO_INC.uri.replace('file://', ''))
+
+    bitBakeProjectScannerClient.bitbakeScanResult = {
+      _layers: [],
+      _overrides: [],
+      _classes: [{
+        name: parsedBazPath.name,
+        path: parsedBazPath,
+        extraInfo: 'layer: core'
+      }],
+      _recipes: [],
+      _includes: [
+        {
+          name: parsedFooPath.name,
+          path: parsedFooPath,
+          extraInfo: 'layer: core'
+        }
+      ]
+    }
+
+    await analyzer.analyze({
+      uri: DUMMY_URI,
+      document: FIXTURE_DOCUMENT.DEFINITION
+    })
+
+    const result = onDefinitionHandler({
+      textDocument: {
+        uri: DUMMY_URI
+      },
+      position: {
+        line: 10,
+        character: 1
+      }
+    })
+
+    expect(result).toEqual(
+      expect.arrayContaining([
+        {
+          uri: FIXTURE_URI.BAZ_BBCLASS,
+          range: {
+            start: {
+              line: 0,
+              character: 0
+            },
+            end: {
+              line: 0,
+              character: 27
+            }
+          }
+        },
+        {
+          uri: FIXTURE_URI.FOO_INC,
+          range: {
+            start: {
+              line: 0,
+              character: 0
+            },
+            end: {
+              line: 0,
+              character: 23
+            }
+          }
+        }
+      ])
+    )
   })
 })
