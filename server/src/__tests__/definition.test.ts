864c9232a18fe3aeaec11efe90f61b5313058619
Ziwei Wang <ziweiwzw217@gmail.com>
2023-11-29 17:20:57 -0500

    Chore: Add tests for go to definition on variables in the current file for both declaration and variable expansion syntax

--- server/src/__tests__/definition.test.ts
+++ server/src/__tests__/definition.test.ts
@@ -154,4 +154,72 @@ describe('on definition', () => {
       ])
     )
   })
+
+  it('provides go to definition for variables found in current file and included files', async () => {
+    const parsedBazPath = path.parse(FIXTURE_DOCUMENT.BAZ_BBCLASS.uri.replace('file://', ''))
+    const parsedFooPath = path.parse(FIXTURE_DOCUMENT.FOO_INC.uri.replace('file://', ''))
+    const parsedBarPath = path.parse(FIXTURE_DOCUMENT.BAR_INC.uri.replace('file://', ''))
+
+    bitBakeProjectScannerClient.bitbakeScanResult = {
+      _layers: [],
+      _overrides: [],
+      _classes: [{
+        name: parsedBazPath.name,
+        path: parsedBazPath,
+        extraInfo: 'layer: core'
+      }],
+      _recipes: [],
+      _includes: [
+        {
+          name: parsedFooPath.name,
+          path: parsedFooPath,
+          extraInfo: 'layer: core'
+        },
+        {
+          name: parsedBarPath.name,
+          path: parsedBarPath,
+          extraInfo: 'layer: core'
+        }
+      ]
+    }
+
+    await analyzer.analyze({
+      uri: DUMMY_URI,
+      document: FIXTURE_DOCUMENT.DIRECTIVE
+    })
+
+    const result1 = onDefinitionHandler({
+      textDocument: {
+        uri: DUMMY_URI
+      },
+      position: {
+        line: 4,
+        character: 1
+      }
+    })
+    // Go to definition for symbols in variable expansion
+    const result2 = onDefinitionHandler({
+      textDocument: {
+        uri: DUMMY_URI
+      },
+      position: {
+        line: 6,
+        character: 11
+      }
+    })
+
+    if (result1 === null) {
+      fail('result1 is null')
+    }
+
+    expect(result1).toEqual(result2)
+    expect(result1).toEqual(
+      expect.arrayContaining([
+        { uri: FIXTURE_URI.FOO_INC, range: { start: { line: 1, character: 0 }, end: { line: 1, character: 21 } } },
+        { uri: FIXTURE_URI.FOO_INC, range: { start: { line: 2, character: 0 }, end: { line: 2, character: 21 } } }, { uri: FIXTURE_URI.BAR_INC, range: { start: { line: 2, character: 0 }, end: { line: 2, character: 21 } } },
+        { uri: DUMMY_URI, range: { start: { line: 4, character: 0 }, end: { line: 4, character: 21 } } },
+        { uri: DUMMY_URI, range: { start: { line: 5, character: 0 }, end: { line: 5, character: 28 } } }
+      ])
+    )
+  })
 })
