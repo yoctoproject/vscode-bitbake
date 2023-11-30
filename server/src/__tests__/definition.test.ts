ed83330558d3cf43b2b633f37741b5e930571f4c
Ziwei Wang <ziweiwzw217@gmail.com>
2023-11-30 13:23:30 -0500

    Chore: Re-implement the test for go to definition for directives; remove duplicate test

--- server/src/__tests__/definition.test.ts
+++ server/src/__tests__/definition.test.ts
@@ -7,20 +7,8 @@ import { analyzer } from '../tree-sitter/analyzer'
 import { generateParser } from '../tree-sitter/parser'
 import { onDefinitionHandler } from '../connectionHandlers/onDefinition'
 import { FIXTURE_DOCUMENT, DUMMY_URI, FIXTURE_URI } from './fixtures/fixtures'
-import { type Location } from 'vscode-languageserver'
-import { definitionProvider } from '../DefinitionProvider'
 import path from 'path'
 import { bitBakeProjectScannerClient } from '../BitbakeProjectScannerClient'
-// TODO: Current implementation of the definitionProvider needs to be improved, this test suite should be modified accordingly after
-const mockDefinition = (path: string | undefined): void => {
-  if (path !== undefined) {
-    const location: Location = { uri: 'file://' + path, range: { start: { line: 0, character: 0 }, end: { line: 0, character: 0 } } }
-
-    jest.spyOn(definitionProvider, 'createDefinitionForKeyword').mockReturnValue(location)
-  } else {
-    jest.spyOn(definitionProvider, 'createDefinitionForKeyword').mockReturnValue([])
-  }
-}
 
 describe('on definition', () => {
   beforeAll(async () => {
@@ -39,91 +27,52 @@ describe('on definition', () => {
     jest.resetAllMocks()
   })
 
-  it('provides definition to directive statement', async () => {
-    await analyzer.analyze({
-      uri: DUMMY_URI,
-      document: FIXTURE_DOCUMENT.DEFINITION
-    })
-
-    let position = {
-      line: 0,
-      character: 9
-    }
-
-    mockDefinition(analyzer.getDocumentTexts(DUMMY_URI)?.[position.line].split(' ')[1])
-
-    const definition1 = onDefinitionHandler({
-      textDocument: {
-        uri: DUMMY_URI
-      },
-      position
-    })
-
-    expect(definition1).toEqual(
-      {
-        uri: 'file://dummy',
-        range: {
-          start: { line: 0, character: 0 },
-          end: { line: 0, character: 0 }
-        }
-      }
-    )
-
-    position = {
-      line: 0,
-      character: 0
-    }
-
-    mockDefinition(analyzer.getDocumentTexts(DUMMY_URI)?.[position.line].split(' ')[1])
-
-    const definition2 = onDefinitionHandler({
-      textDocument: {
-        uri: DUMMY_URI
-      },
-      position
-    })
-
-    expect(definition2).toEqual([])
-  })
-
-  it('provides go to definition for variables if the included files also contain the variable', async () => {
-    const parsedBazPath = path.parse(FIXTURE_DOCUMENT.BAZ_BBCLASS.uri.replace('file://', ''))
+  it('provides go-to-definition to directive statement', async () => {
+    const parsedBarPath = path.parse(FIXTURE_DOCUMENT.BAR_INC.uri.replace('file://', ''))
     const parsedFooPath = path.parse(FIXTURE_DOCUMENT.FOO_INC.uri.replace('file://', ''))
+    const parsedBazPath = path.parse(FIXTURE_DOCUMENT.BAZ_BBCLASS.uri.replace('file://', ''))
 
     bitBakeProjectScannerClient.bitbakeScanResult = {
-      _layers: [],
-      _overrides: [],
-      _classes: [{
-        name: parsedBazPath.name,
-        path: parsedBazPath,
-        extraInfo: 'layer: core'
-      }],
-      _recipes: [],
+      _classes: [
+        {
+          name: parsedBazPath.name,
+          path: parsedBazPath,
+          extraInfo: 'layer: core'
+        }
+      ],
       _includes: [
+        {
+          name: parsedBarPath.name,
+          path: parsedBarPath,
+          extraInfo: 'layer: core'
+        },
         {
           name: parsedFooPath.name,
           path: parsedFooPath,
           extraInfo: 'layer: core'
         }
-      ]
+      ],
+      _layers: [],
+      _overrides: [],
+      _recipes: []
     }
 
     await analyzer.analyze({
-      uri: DUMMY_URI,
-      document: FIXTURE_DOCUMENT.DEFINITION
+      uri: FIXTURE_URI.DIRECTIVE,
+      document: FIXTURE_DOCUMENT.DIRECTIVE
     })
 
-    const result = onDefinitionHandler({
+    const definition = onDefinitionHandler({
       textDocument: {
-        uri: DUMMY_URI
+        uri: FIXTURE_URI.DIRECTIVE
       },
       position: {
-        line: 10,
-        character: 1
+        line: 2,
+        character: 9
       }
     })
 
-    expect(result).toEqual(
+    expect(definition).toEqual(
       expect.arrayContaining([
         {
           uri: FIXTURE_URI.BAZ_BBCLASS,
@@ -134,20 +83,7 @@ describe('on definition', () => {
             },
             end: {
               line: 0,
-              character: 27
-            }
-          }
-        },
-        {
-          uri: FIXTURE_URI.FOO_INC,
-          range: {
-            start: {
-              line: 0,
               character: 0
-            },
-            end: {
-              line: 0,
-              character: 23
             }
           }
         }
