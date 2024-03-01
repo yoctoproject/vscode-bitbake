fd83d16c74d97187eb599388a90524ca09520a37
idillon <io.daza-dillon@savoirfairelinux.com>
2024-03-01 14:34:41 -0500

    Chore: Add unit tests for creation and deletion of 'embedded-documents' folder

---
+++ client/src/__tests__/unit-tests/language/EmbeddedLanguageDocsManager.test.ts
@@ -0,0 +1,22 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) 2023 Savoir-faire Linux. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import fs from 'fs'
+import path from 'path'
+import { embeddedLanguageDocsManager } from '../../../language/EmbeddedLanguageDocsManager'
+
+describe('EmbeddedLanguageDocsManager', () => {
+  it('can create and delete embedded language documents folder', async () => {
+    const storagePath = path.join(__dirname, 'generated-content')
+    await embeddedLanguageDocsManager.setStoragePath(storagePath)
+    expect(embeddedLanguageDocsManager.embeddedLanguageDocsFolder).not.toBeUndefined()
+    if (embeddedLanguageDocsManager.embeddedLanguageDocsFolder === undefined) {
+      return
+    }
+    expect(fs.existsSync(embeddedLanguageDocsManager.embeddedLanguageDocsFolder)).toBeTruthy()
+    await embeddedLanguageDocsManager.deleteEmbeddedLanguageDocsFolder()
+    expect(fs.existsSync(embeddedLanguageDocsManager.embeddedLanguageDocsFolder)).toBeFalsy()
+  })
+})
