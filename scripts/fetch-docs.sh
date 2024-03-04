0a4a34be744fe5fb03280fa7f5647802be57b81c
Ziwei Wang <ziweiwzw217@gmail.com>
2024-03-04 14:22:11 -0500

    Chore: Always fetch the latest commit for docs

--- scripts/fetch-docs.sh
+++ scripts/fetch-docs.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-BITBAKE_DOCS_COMMIT=595176d6be95a9c4718d3a40499d1eb576b535f5
 BITBAKE_DOCS_LIST="bitbake-user-manual-metadata.rst bitbake-user-manual-ref-variables.rst"
 
-YOCTO_DOCS_COMMIT=897d5017eae6b3af2d5d489fc4e0915d9ce21458
 YOCTO_DOCS_LIST=" tasks.rst variables.rst"
 
 set -e
@@ -14,7 +12,7 @@ git clone --depth 1 --filter=blob:none --sparse https://github.com/openembedded/
 cd bitbake
 git sparse-checkout set doc/bitbake-user-manual/
 git fetch origin
-git checkout $BITBAKE_DOCS_COMMIT
+git checkout HEAD
 cd doc/bitbake-user-manual/
 mv $BITBAKE_DOCS_LIST  ../../../client/resources/docs
 cd ../../../
@@ -25,7 +23,7 @@ git clone --depth 1 --filter=blob:none --sparse https://git.yoctoproject.org/yoc
 cd yocto-docs
 git sparse-checkout set documentation/ref-manual
 git fetch origin
-git checkout $YOCTO_DOCS_COMMIT
+git checkout HEAD
 cd documentation/ref-manual
 mv $YOCTO_DOCS_LIST ../../../client/resources/docs
 cd ../../../
