801a4cfc7cef5a49f3769c32b58217ec3b47c1f4
Ziwei Wang <ziweiwzw217@gmail.com>
2024-03-07 11:20:52 -0500

    Chore: Add script to update commit ref in fetch-poky script

--- scripts/fetch-poky.sh
+++ scripts/fetch-poky.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+# Tag: yocto-4.3.3
+COMMIT=d3b27346c3a4a7ef7ec517e9d339d22bda74349d
+
 set -e
 
 mkdir -p resources/poky
 cd resources/poky
 git clone https://github.com/yoctoproject/poky.git .
-git fetch --tags
-TMP_TAG=$(git tag | grep "yocto-" | tail -n 1) # There is a tag: yocto_1.5_M5.rc8 which will take the tail, thus adding a hyphen
-LASTEST_RELEASE=$(git show $TMP_TAG | grep commit | sed "s/^commit //")
 git fetch origin
-git checkout $LASTEST_RELEASE
+git checkout $COMMIT
 
