2fabf53771e6a5aa9e88e0d23064e2deb43ee844
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-03-11 18:03:04 +0800

    Fix: Move resources from client to server

--- scripts/fetch-docs.sh
+++ scripts/fetch-docs.sh
@@ -11,7 +11,7 @@ YOCTO_DOCS_LIST=" tasks.rst variables.rst"
 set -e
 cd "$(dirname "$(readlink -f "$0")")/.."
 
-mkdir -p client/resources/docs
+mkdir -p server/resources/docs
 #  Bitbake docs
 git clone --depth 1 --filter=blob:none --sparse https://github.com/openembedded/bitbake.git
 cd bitbake
@@ -19,7 +19,7 @@ git sparse-checkout set doc/bitbake-user-manual/
 git fetch origin
 git checkout $BITBAKE_DOCS_COMMIT
 cd doc/bitbake-user-manual/
-mv $BITBAKE_DOCS_LIST  ../../../client/resources/docs
+mv $BITBAKE_DOCS_LIST  ../../../server/resources/docs
 cd ../../../
 rm -rf bitbake
 
@@ -30,9 +30,9 @@ git sparse-checkout set documentation/ref-manual
 git fetch origin
 git checkout $YOCTO_DOCS_COMMIT
 cd documentation/ref-manual
-mv $YOCTO_DOCS_LIST ../../../client/resources/docs
+mv $YOCTO_DOCS_LIST ../../../server/resources/docs
 cd ../../../
 rm -rf yocto-docs
 
 # This line is added to let the last task in tasks.rst get matched by the regex in doc scanner
-echo "\n.. _ref-dummy-end-for-matching-do-validate-branches:" >> client/resources/docs/tasks.rst
+echo "\n.. _ref-dummy-end-for-matching-do-validate-branches:" >> server/resources/docs/tasks.rst
