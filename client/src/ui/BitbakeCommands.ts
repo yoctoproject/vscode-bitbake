c1d17cb460042bc0bc2ea0460668188fd7c949ed
Ziwei Wang <ziweiwzw217@gmail.com>
2024-04-26 13:37:37 -0400

    Chore: Remove the code that modifies the saveBeforeRun setting
    Users can always change this setting themselves, no need to modify it.

--- client/src/ui/BitbakeCommands.ts
+++ client/src/ui/BitbakeCommands.ts
@@ -108,12 +108,7 @@ async function parseAllrecipes (bitbakeWorkspace: BitbakeWorkspace, taskProvider
     return
   }
 
-  // Temporarily disable task.saveBeforeRun
-  // This request happens on bitbake document save. We don't want to save all files when any bitbake file is saved.
-  const saveBeforeRun = await vscode.workspace.getConfiguration('task').get('saveBeforeRun')
-  await vscode.workspace.getConfiguration('task').update('saveBeforeRun', 'never', undefined, true)
   await runBitbakeTask(parseAllRecipesTask, taskProvider)
-  await vscode.workspace.getConfiguration('task').update('saveBeforeRun', saveBeforeRun, undefined, true)
 }
 
 async function buildRecipeCommand (bitbakeWorkspace: BitbakeWorkspace, bitBakeProjectScanner: BitBakeProjectScanner, uri?: any): Promise<void> {
@@ -166,12 +161,7 @@ async function scanRecipeCommand (bitbakeWorkspace: BitbakeWorkspace, taskProvid
     return
   }
 
-  // Temporarily disable task.saveBeforeRun
-  // This request happens on bitbake document save. We don't want to save all files when any bitbake file is saved.
-  const saveBeforeRun = await vscode.workspace.getConfiguration('task').get('saveBeforeRun')
-  await vscode.workspace.getConfiguration('task').update('saveBeforeRun', 'never', undefined, true)
   await bitbakeRecipeScanner.scan(chosenRecipe, taskProvider, uri)
-  await vscode.workspace.getConfiguration('task').update('saveBeforeRun', saveBeforeRun, undefined, true)
 }
 
 async function runTaskCommand (bitbakeWorkspace: BitbakeWorkspace, bitBakeProjectScanner: BitBakeProjectScanner, client: LanguageClient, uri?: any, task?: any): Promise<void> {
