f60f9c98d89367fb1a831ba122dc4ae566fa69e8
idillon <io.daza-dillon@savoirfairelinux.com>
2024-05-15 18:51:59 -0400

    Chore: Add script to update tree-sitter-bash wasm

--- scripts/update-tree-sitter-wasm.sh
+++ scripts/update-tree-sitter-wasm.sh
@@ -4,14 +4,20 @@
 # Reference: https://github.com/bash-lsp/bash-language-server/blob/8c42218c77a9451b308839f9a754abde901323d5/scripts/upgrade-tree-sitter.sh
 
 set -euox pipefail
-cd "$(dirname "$(readlink -f "$0")")/.."
 
-cd server
-npm install -D tree-sitter-cli https://github.com/tree-sitter-grammars/tree-sitter-bitbake
-npx tree-sitter build-wasm node_modules/tree-sitter-bitbake
+update-tree-sitter-wasm() {
+    local repo_name=$1
+    local repo_owner=$2
+    local info_file="$repo_name.info"
 
-curl 'https://api.github.com/repos/tree-sitter-grammars/tree-sitter-bitbake/commits/master' | jq .commit.url > parser.info
-echo "tree-sitter-cli $(cat package.json | jq '.devDependencies["tree-sitter-cli"]')" >> parser.info
+    cd "$(dirname "$(readlink -f "$0")")/.."
 
-npm uninstall tree-sitter-cli tree-sitter-bitbake
+    cd server
+    npm install -D tree-sitter-cli "https://github.com/$repo_owner/$repo_name"
+    npx tree-sitter build-wasm "node_modules/$repo_name"
 
+    curl "https://api.github.com/repos/$repo_owner/$repo_name/commits/master" | jq .commit.url > "$info_file"
+    echo "tree-sitter-cli $(cat package.json | jq '.devDependencies["tree-sitter-cli"]')" >> "$info_file"
+
+    npm uninstall tree-sitter-cli "$repo_name"
+}
