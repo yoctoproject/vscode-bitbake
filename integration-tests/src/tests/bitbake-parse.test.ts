3776dced771d1e2c453873e5cee6fb7ae4a216e9
Enguerrand de Ribaucourt <enguerrand.de-ribaucourt@savoirfairelinux.com>
2024-05-31 10:28:53 +0200

    CI: Copy error recipe to poky instead of using an extra layer
    Adding a layer is more complex and requires a full recipe parsing.
    Just copying the recipe we want to parse is a lot faster.

--- integration-tests/src/tests/bitbake-parse.test.ts
+++ integration-tests/src/tests/bitbake-parse.test.ts
@@ -7,27 +7,28 @@ import * as assert from 'assert'
 import * as vscode from 'vscode'
 
 import path from 'path'
-import { BITBAKE_TIMEOUT, addLayer, excludeRecipes, resetExcludedRecipes, awaitBitbakeParsingResult, resetLayer } from '../utils/bitbake'
+import { BITBAKE_TIMEOUT, awaitBitbakeParsingResult, importRecipe, removeRecipe } from '../utils/bitbake'
 import { assertWillComeTrue, assertWorkspaceWillBeOpen } from '../utils/async'
 
 suite('Bitbake Parsing Test Suite', () => {
   let workspaceURI: vscode.Uri
-  let buildFolder: vscode.Uri
+  let errorRecipePath: string
+  let workspacePath: string
+  let pokyPath: string
 
   suiteSetup(async function (this: Mocha.Context) {
     this.timeout(BITBAKE_TIMEOUT)
     await assertWorkspaceWillBeOpen()
     workspaceURI = (vscode.workspace.workspaceFolders as vscode.WorkspaceFolder[])[0].uri
-    buildFolder = vscode.Uri.joinPath(workspaceURI, 'build')
-
-    // Generate the build directory for the addLayer functions to work
-    await vscode.commands.executeCommand('bitbake.parse-recipes')
-    await awaitBitbakeParsingResult()
+    errorRecipePath = path.resolve(__dirname, '../../project-folder/sources/meta-error/recipes-error/error/unparsed-line.bb')
+    workspacePath = workspaceURI.fsPath
+    pokyPath = path.resolve(workspacePath, 'sources/poky')
   })
 
-  suiteTeardown(async function (this: Mocha.Context) {
-    this.timeout(10000)
-    await vscode.workspace.fs.delete(buildFolder, { recursive: true })
+  suiteTeardown(async () => {
+    try {
+      await removeRecipe(errorRecipePath, pokyPath)
+    } catch (e) {}
   })
 
   test('Bitbake can succesfully parse poky', async () => {
@@ -39,31 +40,14 @@ suite('Bitbake Parsing Test Suite', () => {
   }).timeout(BITBAKE_TIMEOUT)
 
   test('Bitbake can detect parsing errors', async () => {
-    const recipesToExclude: string[] = [
-      'recipes-error/error/compilation-python-function.bb',
-      'recipes-error/error/execution-python-function.bb',
-      'recipes-error/error/unable-to-parse.bb',
-      'recipes-error/error/unparsed-line.bb',
-      'recipes-error/error/task-error.bb',
-      'recipes-error/error/variable-error.bb',
-      'recipes-error/error/non-existent-uri.bb'
-    ]
-
-    const workspacePath: string = workspaceURI.fsPath
-    await addLayer(path.resolve(__dirname, '../../project-folder/sources/meta-error'), workspacePath)
-
-    // Everything is excluded except the one that is filtered out
-    await excludeRecipes(recipesToExclude.filter((recipe) => recipe !== 'recipes-error/error/unparsed-line.bb'), workspacePath)
+    await importRecipe(errorRecipePath, pokyPath)
 
     await vscode.commands.executeCommand('bitbake.parse-recipes')
     await awaitBitbakeParsingResult()
 
-    await resetLayer(path.resolve(__dirname, '../../project-folder/sources/meta-error'), workspacePath)
-
-    await resetExcludedRecipes(workspacePath)
-
     await assertWillComeTrue(async () => {
       const diagnostics = vscode.languages.getDiagnostics()
+
       // Only 1 file has problem(s)
       if (diagnostics.length !== 1) {
         return false
@@ -73,7 +57,7 @@ suite('Bitbake Parsing Test Suite', () => {
         return false
       }
 
-      if (!diagnostics[0][0].path.includes('recipes-error/error/unparsed-line.bb')) {
+      if (!diagnostics[0][0].path.includes('recipes-core/base-files/unparsed-line.bb')) {
         return false
       }
       if (!diagnostics[0][1][0].message.includes('unparsed line: \'undefinedvariable\'')) {
@@ -82,5 +66,7 @@ suite('Bitbake Parsing Test Suite', () => {
 
       return true
     })
+
+    await removeRecipe(errorRecipePath, pokyPath)
   }).timeout(BITBAKE_TIMEOUT)
 })
--- integration-tests/src/utils/bitbake.ts
+++ integration-tests/src/utils/bitbake.ts
@@ -5,29 +5,9 @@
 
 import * as vscode from 'vscode'
 import { assertWillComeTrue } from './async'
+import path from 'path'
 
 export const BITBAKE_TIMEOUT = 300000
-
-// TODO If bitbake-layers vscode commands are added in the future, use them instead
-export async function addLayer (layer: string, workspaceFolder: string): Promise<void> {
-  const bblayersConf = getBBlayersConfUri(workspaceFolder)
-  const bblayersConfContent = await vscode.workspace.fs.readFile(bblayersConf)
-  let fileContent = bblayersConfContent.toString()
-  fileContent += `\nBBLAYERS+="${layer}"\n`
-  await vscode.workspace.fs.writeFile(bblayersConf, Buffer.from(fileContent))
-}
-
-// Replace with remove-layer command if available
-export async function resetLayer (layer: string, workspaceFolder: string): Promise<void> {
-  const bblayersConf = getBBlayersConfUri(workspaceFolder)
-  const bblayersConfContent = await vscode.workspace.fs.readFile(bblayersConf)
-
-  // Remove last line
-  const lines = bblayersConfContent.toString().split('\n')
-  lines.pop()
-  const fileContent = lines.join('\n')
-  await vscode.workspace.fs.writeFile(bblayersConf, Buffer.from(fileContent))
-}
 
 /// Wait for the bitbake parsing task to finish
 export async function awaitBitbakeParsingResult (): Promise<void> {
@@ -41,28 +21,13 @@ export async function awaitBitbakeParsingResult (): Promise<void> {
   disposable.dispose()
 }
 
-export async function excludeRecipes (recipes: string[], workspaceFolder: string): Promise<void> {
-  const bblayersConfUri = getBBlayersConfUri(workspaceFolder)
-  const bblayersConfContent = await vscode.workspace.fs.readFile(bblayersConfUri)
-  let fileContent = bblayersConfContent.toString()
-  for (const recipe of recipes) {
-    fileContent += `\nBBMASK+="${recipe}"\n`
-  }
-  await vscode.workspace.fs.writeFile(bblayersConfUri, Buffer.from(fileContent))
+/// Copy a recipe into poky
+export async function importRecipe (recipePath: string, pokyPath: string): Promise<void> {
+  const pokyDestinationPath = path.resolve(pokyPath, 'meta/recipes-core/base-files', path.basename(recipePath))
+  await vscode.workspace.fs.copy(vscode.Uri.file(recipePath), vscode.Uri.file(pokyDestinationPath))
 }
 
-export async function resetExcludedRecipes (workspaceFolder: string): Promise<void> {
-  const bblayersConfUri = getBBlayersConfUri(workspaceFolder)
-  const bblayersConfContent = await vscode.workspace.fs.readFile(bblayersConfUri)
-
-  const lines = bblayersConfContent.toString().split('\n')
-  const fileContentWithoutBBMASK = lines.filter((line) => (
-    !line.includes('BBMASK')
-  )).join('\n')
-  await vscode.workspace.fs.writeFile(bblayersConfUri, Buffer.from(fileContentWithoutBBMASK))
-}
-
-function getBBlayersConfUri (workspaceFolder: string): vscode.Uri {
-  const buildFolder = vscode.Uri.joinPath(vscode.Uri.file(workspaceFolder), 'build')
-  return vscode.Uri.joinPath(buildFolder, 'conf/bblayers.conf')
+export async function removeRecipe (recipePath: string, pokyPath: string): Promise<void> {
+  const pokyDestinationPath = path.resolve(pokyPath, 'meta/recipes-core/base-files', path.basename(recipePath))
+  await vscode.workspace.fs.delete(vscode.Uri.file(pokyDestinationPath))
 }
