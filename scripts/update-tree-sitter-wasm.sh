ec5a34939da613c531ce60ded74cb9c0194b1abd
idillon <io.daza-dillon@savoirfairelinux.com>
2024-06-18 16:27:07 -0400

    Chore: Install wasm files with script

--- scripts/update-tree-sitter-wasm.sh
+++ scripts/update-tree-sitter-wasm.sh
@@ -13,11 +13,6 @@ update-tree-sitter-wasm() {
     cd "$(dirname "$(readlink -f "$0")")/.."
 
     cd server
-    npm install -D tree-sitter-cli "https://github.com/$repo_owner/$repo_name"
-    npx tree-sitter build-wasm "node_modules/$repo_name"
-
     curl "https://api.github.com/repos/$repo_owner/$repo_name/commits/master" | jq .commit.url > "$info_file"
-    echo "tree-sitter-cli $(cat package.json | jq '.devDependencies["tree-sitter-cli"]')" >> "$info_file"
-
-    npm uninstall tree-sitter-cli "$repo_name"
+    echo "tree-sitter-cli $(npm show tree-sitter-cli version)" >> "$info_file"
 }
