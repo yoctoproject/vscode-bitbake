6e0225a2c3583716d1b2d3368d4408a9d0a8f190
idillon <io.daza-dillon@savoirfairelinux.com>
2024-09-17 11:47:42 -0400

    Fix: eslint issues on server/src/completions/snippet-utils.ts

--- server/src/completions/snippet-utils.ts
+++ server/src/completions/snippet-utils.ts
@@ -33,6 +33,7 @@ export function formatCompletionItems (completions: CompletionItem[], completion
       kind: item.kind ?? completionItemKind ?? CompletionItemKind.Snippet
     }
 
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { data, ...filtered } = formatted
 
     return filtered
